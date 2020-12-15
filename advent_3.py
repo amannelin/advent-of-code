1dac23087187d804cfd5232a3ff830e2608e5692
Abigail <amannelin@gmail.com>
2020-12-15 11:16:42 -0600

    starting day 4

--- advent_3.py
+++ advent_3.py
@@ -1,28 +1,27 @@
-def check_tobaggan_route(file):
+def check_tobaggan_route(file, x_increment, y_increment):
     map = open(file)
+    map = map.readlines()
     x = 0
+    y = 0
     trees = 0
 
-    for line in map:
-        line.rstrip()
-        if line[x % 31] == "#":
+    while y < len(map):
+        if map[y][x % 31] == "#":
             trees += 1
-        x += 3
+        x += x_increment
+        y += y_increment
     
     return trees
 
-print(check_tobaggan_route("advent3.txt"))
+print(check_tobaggan_route("advent3.txt", 3, 1) *
+        check_tobaggan_route("advent3.txt", 1, 1) *
+        check_tobaggan_route("advent3.txt", 5, 1) *
+        check_tobaggan_route("advent3.txt", 7, 1) *
+        check_tobaggan_route("advent3.txt", 1, 2))
 
-def check_more_routes(file):
-    map = open(file)
-    x = 0
-    trees= 0
-    y = [1, 3, 5, 7]
-Right 1, down 1.
-Right 3, down 1. (This is the slope you already checked.)
-Right 5, down 1.
-Right 7, down 1.
-    for line in map:
-        line.rstrip()
-        if line [x % 31]
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
         
